e60dc9a960b69d4b072416985a7d851d3897f919
ANS <C@MacBookPro.lan>
2025-07-20 11:48:33 -0400

    i put my name

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
  return {
      'statusCode': 200,
-     'body': json.dumps('Hello lambda from vs code ')
+     'body': json.dumps('Hello lambda from vs code this is Ans ')
     }
